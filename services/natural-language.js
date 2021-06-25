930234df3ad2b23c08b014c65ee105366c885b2e
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-06-25 12:55:24 +0100

    updated services to use vcap environment variable in production

--- services/natural-language.js
+++ services/natural-language.js
@@ -5,18 +5,12 @@ const Config = require('../config');
 class NaturalLanguage {
     constructor() {
         const config = new Config()
-        if (process.env.NODE_ENV == 'production') {
-            const appEnv = config.getAppEnv()
-            this.nlu = new NaturalLanguageUnderstandingV1(appEnv.services['natural-language-understanding'][0].credentials);
-        }
-        else {
-            config.getNaturalLanguageConfig()
+            let nlConfig = config.getNaturalLanguageConfig()
             this.nlu = new NaturalLanguageUnderstandingV1({
-                authenticator: new IamAuthenticator({ apikey: config.apiKey }),
+                authenticator: new IamAuthenticator({ apikey: nlConfig.apiKey }),
                 version: '2018-04-05',
                 serviceUrl: 'https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/9ed65066-5926-4a4a-b795-8448ae5c335c'
             });
-        }
 
     }
 
--- services/tone-analyser.js
+++ services/tone-analyser.js
@@ -5,19 +5,14 @@ const Config = require('../config');
 class ToneAnalyser {
     constructor() {
         const config = new Config()
-        if (process.env.NODE_ENV == 'production') {
-            const appEnv = config.getAppEnv()
-            this.toneAnalyzer = new ToneAnalyzerV3(appEnv.services['tone_analyser'][0].credentials)
-        } else {
-            config.getToneAnalyserConfig()
+            let taConfig = config.getToneAnalyserConfig()
             this.toneAnalyzer = new ToneAnalyzerV3({
                 version: '2017-09-21',
                 authenticator: new IamAuthenticator({
-                    apikey: config.apiKey,
+                    apikey: taConfig.apiKey,
                 }),
                 serviceUrl: 'https://api.eu-gb.tone-analyzer.watson.cloud.ibm.com/instances/c080430e-be19-4be4-8e67-505c98acd04a',
             });
-        }
     }
     analyseDescription(description) {
         return this.toneAnalyzer.tone({ toneInput: { text: description }, contentType: 'application/json', })
